9c8d4dabd64cbb4b463e8159006c8aec3375b7dc
Velsee <113857212+Velsee@users.noreply.github.com>
2023-04-13 23:58:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,28 +4,31 @@ from scipy import stats
 
 chat_id = 1253313260 # Ваш chat ID, не меняйте название переменной
 
-def solution(control: np.array, test: np.array) -> bool:
-    alpha = 0.01  # Установим уровень значимости
-    t_stat, p_value = stats.ttest_ind(control, test)  # Сравниваем выборки
-
-    # Если p_value меньше уровня значимости, отклоняем нулевую гипотезу
-    if p_value < alpha:
-        return True
-    else:
-        return False
-
-
 # Выполняем скрипт с данными в формате .csv
 url = "https://raw.githubusercontent.com/Velsee/Data/main/hyp3_historical_data.csv"
 
 # Считываем данные из файла
-df = pd.read_csv(url)
+data = pd.read_csv(url)
 
-# Формируем массив для анализа
-control_data = df[df["group"] == "control"]["NPV"].values
-test_data = df[df["group"] == "test"]["NPV"].values
+control_data = data.loc[data["group"] == "control"]
+test_data = data.loc[data["group"] == "test"]
 
-# Вызываем функцию и выводим ответ
-answer = solution(control_data, test_data)
-print("Отклонить ли нулевую гипотезу?", answer)
+# Целевая переменная, предположительно "NPV"
+control_NPV = control_data["NPV"]
+test_NPV = test_data["NPV"]
 
+# Функция для проверки статистической значимости
+def solution(control_NPV: np.array, test_NPV: np.array, alpha: float) -> bool:
+    t_stat, p_val = stats.ttest_ind(control_NPV[:500], test_NPV[:500], equal_var=False)
+    
+    # Отклонить нулевую гипотезу, если p-значение меньше заданного уровня значимости
+    reject_null_hypothesis = p_val < alpha
+    
+    return reject_null_hypothesis
+
+# Уровень значимости критерия
+alpha = 0.01
+
+result = solution(control_NPV, test_NPV, alpha)
+
+print("Отклонить ли нулевую гипотезу:", result)
